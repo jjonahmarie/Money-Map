b7b71cd16b7a60ce3107e5b70aa0184b60636893
Jonah Evangelista <jonahevangelista@yahoo.com>
2022-11-18 23:25:16 -0700

    Initial Commit

---
+++ Money Map/Money_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_MapApp.swift
+//  Money Map
+//
+//  Created by Jonah Marie Evangelista on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct Money_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
